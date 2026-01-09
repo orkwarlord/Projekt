6e5225cb9a2ac4269f14c77c91a303b3aaafbece
Thran <Thran@LAPTOP-G5JUPH0P>
2026-01-09 18:45:40 +0100

    Initial

---
+++ Projekt/DTO/BookDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Projekt.DTO
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public Category? Category { get; set; }
+
+        public 
+    }
+}
--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -23,5 +23,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
