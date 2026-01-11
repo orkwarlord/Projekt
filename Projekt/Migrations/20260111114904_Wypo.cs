b89251a45c504881afd7b1d03ed6b1e46c0d8cc2
Thran <Thran@LAPTOP-G5JUPH0P>
2026-01-11 12:49:31 +0100

    Migracja

---
+++ Projekt/Migrations/20260111114904_Wypo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class Wypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueAt",
+                table: "Rentings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueAt",
+                table: "Rentings",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
