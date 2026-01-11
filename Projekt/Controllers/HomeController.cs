0c13d8a04a10a43ddf62e3612b288d841a604dfa
Mikolaj1101 <pzx120848@student.poznan.merito.pl>
2026-01-11 14:37:20 +0100

    statystyki biblioteki w home controller

--- Projekt/Controllers/HomeController.cs
+++ Projekt/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projekt.Data;
 using Projekt.Models;
 
 namespace Projekt.Controllers
@@ -7,14 +9,20 @@ namespace Projekt.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.BooksCount = await _context.Books.CountAsync();
+            ViewBag.CategoriesCount = await _context.Categories.CountAsync();
+            ViewBag.ActiveRentingsCount = await _context.Rentings.CountAsync(r => r.ReturnedAt == null);
+
             return View();
         }
 
